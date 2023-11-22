854b632a110373f669fe7a7a5022ea41d1d4fc01
dvanwolffelaar <daanblablavw@gmail.com>
2023-11-22 17:21:00 +0100

    Constr1 defined

--- constraints/constr3.py
+++ constraints/constr3.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def plane_assigned_to_only_one_gate(x, I) -> np.ndarray:
     """
+    Condition checks if aircraft is assigned to strictly one gate
     :param x: numpy array: 1 if aircraft 𝑖 is assigned to gate 𝑘, and 0 otherwise.
     :type x: np.ndarray
     :param I: set of all domestic flights
