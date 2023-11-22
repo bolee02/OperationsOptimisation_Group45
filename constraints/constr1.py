e7b8be0b612609bafa6fa323784c7677c5659a21
dvanwolffelaar <daanblablavw@gmail.com>
2023-11-22 17:10:19 +0100

    Constr1 defined

--- constraints/constr1.py
+++ constraints/constr1.py
@@ -1,8 +1,16 @@
 import numpy as np
 
-def gate_assigned_to_only_one_gate(x, K, I):
-    array = np.array((len(I), 1))
+
+def gate_assigned_to_only_one_gate(x, I):
+    """
+    :param x: numpy array: 1 if aircraft 𝑖 is assigned to gate 𝑘, and 0 otherwise.
+    :type x:
+    :param I:
+    :return:
+    """
+    array = np.array((len(I), 1), dtype=bool)
     for i in range(len(I)):
         array[i] = sum(x[i]) == 1
 
+    return array
 
