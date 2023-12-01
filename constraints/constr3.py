3551ac9aad31ff9200006535078423d120265ea9
dvanwolffelaar <daanblablavw@gmail.com>
2023-12-01 19:23:15 +0100

    Constraint 6 appears to be working

--- constraints/constr3.py
+++ constraints/constr3.py
@@ -2,18 +2,6 @@ import numpy as np
 from gurobipy import quicksum
 
 
-def plane_assigned_to_only_one_gate(x) -> np.ndarray:
-    """
-    Condition checks if aircraft is assigned to strictly one gate
-    :param x: numpy array: 1 if aircraft 𝑖 is assigned to gate 𝑘, and 0 otherwise.
-    :type x: np.ndarray
-    :param I: set of all domestic flights
-    :type I: np.ndarray
-    :return: array which is true if the condition is made, otherwise false
-    """
-    return np.sum(x, axis=0) == 1
-
-
-def plane_assigned_to_only_one_gate_dict(x, K_i, K_d):
-    return (quicksum(x[:, k]) for k in range(len(K_d.keys())+len(K_i.keys())))
+def plane_assigned_to_only_one_gate_dict(x, i, K):
+    return quicksum(x[i, k] for k in range(K)) == 1
 
--- constraints/constr6.py
+++ constraints/constr6.py
@@ -12,7 +12,7 @@ def number_of_aircraft_in_the_apron(x, K_d, I_d, I_i, K_i):
     return (quicksum(x[len(K_d.keys())+len(K_i.keys())+1, i]) for i in range(len(I_d.keys())+len(I_i.keys()))) == NA
 
 
-def find_number_in_apron(K, I):
+def find_number_in_apron(K, I, It):
     '''
     :param K:
     :type K: dict
@@ -26,32 +26,40 @@ def find_number_in_apron(K, I):
     model = Model()
     Z = {}
 
-    for i in range(I_len):
-        for j in range(I_len):
-            Z[i, j] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name=f'x_{i}{j}')
+    for i in range(1, I_len+1):  # Arcs from 0th node to all other nodes
+        Z[0, i] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name=f'x_{0}{i}')
+    for i in range(I_len):  # Arcs to |I|th node from all other nodes
+        Z[i, I_len] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name=f'x_{i}{I_len}')
+
+    for i in range(1, I_len):
+        for j in range(1, I_len):
+            if not any(all(aircraft in list(It_x.values()) for aircraft in [I[i], I[j]]) for It_x in list(It.values())) and i != j:
+                Z[i, j] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name=f'x_{i}{j}')
+            else:
+                Z[i, j] = 0
 
     model.update()
-    A = (quicksum(Z[0, j] for j in range(I_len)) <= K_len)
-    C1 = model.addConstr((quicksum(Z[0, j] for j in range(I_len)) <= K_len), name="C1")
-    B = (quicksum(Z[i, I_len-1] for i in range(I_len)) <= K_len)
-    C2 = model.addConstr((quicksum(Z[i, I_len-1] for i in range(I_len)) <= K_len), name="C2")
-    C = (quicksum(Z[i, j]) == quicksum(Z[j, i]) for j in range(I_len`) for i in range(I_len))
-    C3 = model.addConstr((quicksum(Z[i, :]) == quicksum(Z[:, i]) for i in range(I_len)), name="C3")
-    D = ((quicksum(Z[i, j]) for j in range(I_len)) for i in range(I_len))
-    C4 = model.addConstr(((quicksum(Z[i, j]) for j in range(I_len)) for i in range(I_len)), name="C4")
+    C1 = model.addConstr((quicksum(Z[0, j] for j in range(1, I_len+1)) <= K_len), name="C1")
+    C2 = model.addConstr((quicksum(Z[i, I_len] for i in range(I_len)) <= K_len), name="C2")
+
+    for i in range(I_len):
+        C3 = model.addConstr((quicksum(Z[i, j] for j in range(1, I_len+1)) == quicksum(Z[j, i+1] for j in range(I_len))), name=f"C{3+i}")
+    for i in range(I_len):
+        C4 = model.addConstr((quicksum(Z[i, j] for j in range(1, I_len+1)) <= 1), name=f"C{3+I_len+1}")
 
     obj = LinExpr()
     for i in range(I_len):
-        for j in range(I_len):
+        for j in range(1, I_len+1):
             obj += Z[i, j]
 
     model.setObjective(obj, GRB.MAXIMIZE)
     model.update()
     model.optimize()
-    return I_len - 1 - Model.getObjective().getValue()
+    return I_len - 1 - model.ObjVal
 
 
 a = {1: "a", 2: "b", 3: "c"}  # I
 b = {1: "a", 2: "b"}  # K
+c = {0: a}
 
-print(find_number_in_apron(b, a))
+print(find_number_in_apron(b, a, c))
